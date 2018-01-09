a55ef2c22a0fb4c8af70665030762c5d1a71153b
NAPIER-MAIL\40206111 <40206111@live.napier.ac.uk>
2018-01-09 15:00:53 +0000

    0.1.7 -Fixed Typo in main.cpp -Fixed Type in CMakeLists

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -7,7 +7,7 @@ int main(){
 	
 	while (window.isOpen()){
 		sf::Event event;
-		while (windo.pollEvent(event)){
+		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed){
 				window.close();
 			}
