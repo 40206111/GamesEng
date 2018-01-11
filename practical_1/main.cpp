4a422772b05477feaa85229e7493aba84e1c8b73
NAPIER-MAIL\40206111 <40206111@live.napier.ac.uk>
2018-01-11 12:54:20 +0000

    CMake -Copies res folder
    Pong
    -Changed paddle [1]'s start position

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -64,7 +64,7 @@ void Load()
 	ball.setOrigin(ballRadius / 2, ballRadius / 2);
 	
 	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-	paddles[1].setPosition(gameWidth - paddleSize.x, gameHeight / 2);
+	paddles[1].setPosition(gameWidth - (10 + paddleSize.x/2), gameHeight / 2);
 	
 
 	font.loadFromFile("res/font/RobotoMono-Regular.ttf");
