e5e0642693def56f84f1783769bca2ae502b2cdf
NAPIER-MAIL\40206111 <40206111@live.napier.ac.uk>
2018-03-20 15:23:08 +0000

    Maze -Finished Maths library -Integrated level sytem into game

--- practical_3/Player.cpp
+++ practical_3/Player.cpp
@@ -1,24 +1,50 @@
 #include "Player.h"
+#include "levelsystem.h"
 using namespace sf;
 using namespace std;
+
+bool validmove(Vector2f pos)
+{
+	return (ls::getTileAt(pos) != ls::WALL);
+}
 
 void Player::update(double dt)
 {
 	if (Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))
 	{
-		move(Vector2f(100 * dt, 0));
+		Vector2f testpos = this->getPosition() + Vector2f(100 * dt, 0);
+		testpos.x += 25;
+		if (validmove(testpos))
+		{
+			move(Vector2f(100 * dt, 0));
+		}
 	}
 	if (Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))
 	{
-		move(Vector2f(-100 * dt, 0));
+		Vector2f testpos = this->getPosition() + Vector2f(-100 * dt, 0);
+		testpos.x -= 25;
+		if (validmove(testpos))
+		{
+			move(Vector2f(-100 * dt, 0));
+		}
 	}
 	if (Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up))
 	{
-		move(Vector2f(0, -100 * dt));
+		Vector2f testpos = this->getPosition() + Vector2f(0, -100 * dt);
+		testpos.y += 5;
+		if (validmove(testpos))
+		{
+			move(Vector2f(0, -100 * dt));
+		}
 	}
 	if (Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down))
 	{
-		move(Vector2f(0, 100 * dt));
+		Vector2f testpos = this->getPosition() + Vector2f(0, 100 * dt);
+		testpos.y += 55;
+		if (validmove(testpos))
+		{
+			move(Vector2f(0, 100 * dt));
+		}
 	}
 	Entity::update(dt);
 }
@@ -33,3 +59,4 @@ void Player::render(sf::RenderWindow &window) const
 {
 	window.draw(*_shape);
 }
+
--- practical_3/main.cpp
+++ practical_3/main.cpp
@@ -1,15 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include "Entity.h"
 #include "Player.h"
+#include "levelsystem.h"
 using namespace sf;
 using namespace std;
 
 int gameWidth = 800;
-int gameHeight = 600;
+int gameHeight = 500;
 std::vector<Entity *> entities;
 
 void Render(RenderWindow &window)
 {
+	ls::render(window);
 	for (auto &e : entities)
 	{
 		e->render(window);
@@ -20,6 +22,23 @@ void Load()
 {
 	Entity *player = new Player();
 	entities.push_back(player);
+
+	ls::loadLevelFile("res/maze_2.txt");
+
+	//print the level to console
+	for (size_t y = 0; y < ls::getHeight(); ++y)
+	{
+		for (size_t x = 0; x < ls::getWidth(); ++x)
+		{
+			cout << ls::getTile({ x, y });
+			if (ls::getTile({ x,y }) == 1)
+			{
+				Vector2f pos = ls::getTilePosition(sf::Vector2ul(x, y));
+				entities[0]->setPosition(Vector2f(pos.x + 50, pos.y + 50));
+			}
+		}
+		cout << endl;
+	}
 }
 
 void Update(RenderWindow &window)
